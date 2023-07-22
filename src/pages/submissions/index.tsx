5f7fe1a8ba63834566f968cb8b0522e89d0e5b4c
Lokprakash-babu <lokprakash100@gmail.com>
2023-07-22 08:54:25 +0530

    auth addition

--- src/pages/submissions/index.tsx
+++ src/pages/submissions/index.tsx
@@ -4,16 +4,40 @@ import PageHead from "@/components/PageHead/PageHead";
 import PageWrapper from "@/components/PageWrapper/PageWrapper";
 import { useSidebarContext } from "@/components/Sidebar";
 import SubmissionsTable from "@/components/Submission/SubmissionsTable";
+import Unauthenticated from "@/components/Unauthenticated";
 import { Button, Result } from "antd";
 import Link from "next/link";
 import { useEffect } from "react";
 
 const Submissions = () => {
-  const { authLoading, authenticatedUser } = useAuth();
+  const {
+    authLoading,
+    authenticatedUser,
+    isUserLoggedIn,
+    setAuthenticatedUser,
+    setAuthLoading,
+  } = useAuth();
   const { expandSidebar } = useSidebarContext();
+
   useEffect(() => {
     expandSidebar?.();
   }, []);
+
+  const whoAmI = async () => {
+    try {
+      await isUserLoggedIn?.();
+    } catch (e) {
+      setAuthenticatedUser?.(null);
+      setAuthLoading?.(false);
+    }
+  };
+  useEffect(() => {
+    whoAmI();
+  }, []);
+
+  if (!authenticatedUser?.username && !authLoading) {
+    return <Unauthenticated />;
+  }
   if (authLoading) return <Loader />;
   if (!authenticatedUser)
     return (
