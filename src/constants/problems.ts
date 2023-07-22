a1218a4bae75941dfc062292e12888eb46dc6ea2
Lokprakash-babu <lokprakash100@gmail.com>
2023-07-22 08:18:57 +0530

    prompt correction

--- src/constants/problems.ts
+++ src/constants/problems.ts
@@ -9,14 +9,12 @@ export const problems = {
     },
     difficulty: "Easy",
     problemDescription:
-      "How can customer support effectively address Joey's complaint regarding a spoiled pizza and provide a satisfactory resolution, including a refund, while maintaining a positive customer experience and restoring trust in the brand?",
+      "How can customer support effectively address a complaint regarding the spoiled pizza and provide a satisfactory resolution, including a refund, while maintaining a positive customer experience and restoring trust in the brand?",
     expectations: [
-      "Active listening: The customer support agent should listen attentively to Joey's concerns, allowing him to express his frustration and ensuring he feels heard and understood.",
-      "Empathy and understanding: The agent should demonstrate empathy towards Joey's situation, acknowledging his disappointment and anger caused by receiving a spoiled pizza.",
-      "Apology and taking responsibility: The agent should offer a sincere apology on behalf of the company, taking responsibility for the issue and assuring Joey that his complaint is being taken seriously.",
+      "Active listening: The customer support agent should listen attentively to the concerns, allowing customers to express their frustration and ensuring they feel heard and understood.",
+      "Empathy and understanding: The agent should demonstrate empathy towards the situation, acknowledging customer's disappointment and anger caused by receiving a spoiled pizza.",
       "Timely response and resolution: The agent should aim to resolve the issue promptly, providing timely updates to Joey and keeping him informed of the progress made towards finding a solution.",
-      "Solution-oriented approach: The agent should actively work towards finding a satisfactory resolution, including offering a refund or a replacement pizza, as per Joey's preference.",
-      "Effective communication: The agent should communicate clearly and professionally, ensuring that Joey understands the proposed solution, any necessary steps he needs to take, and the expected timeline for resolution.",
+      "Clear and concise: You must not exceed more than 10 messages.",
     ],
     context:
       "You are an angry customer who didn't get proper pizza delivered. You are in conversation with support agent of that particular company, you have to keep your messages short and concise not exceeding 70 words",
